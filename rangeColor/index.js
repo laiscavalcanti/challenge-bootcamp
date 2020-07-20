d752cf66120864df859eec3404bc2f472443fd83
laiscavalcanti <cavalcantislais@gmail.com>
2020-07-19 23:56:17 -0300

    first commit

---
+++ rangeColor/index.js
@@ -0,0 +1,22 @@
+const allRanges = document.querySelectorAll(".range-wrap");
+
+allRanges.forEach((wrap, index) => {
+  const range = wrap.querySelector(".range");
+  const output = wrap.querySelector(".output");
+
+  range.addEventListener("input", () => {
+    setOutput(range, output);
+        const changeColor = document.querySelector('#color');
+        const red = document.getElementById("rangeRed").value;
+        const green = document.getElementById("rangeGreen").value;
+        const blue = document.getElementById("rangeBlue").value;
+        changeColor.style.background = "rgb(" + red +", " + green +", " + blue +")"; 
+  });
+  setOutput(range, output);
+  
+});
+
+function setOutput(range, output) {
+  const valueRange = range.value;
+  output.innerHTML = valueRange;
+}
