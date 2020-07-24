ffeac52e4ddf90397346c1a2f779904efa04fbef
laiscavalcanti <cavalcantislais@gmail.com>
2020-07-24 01:25:21 -0300

    precisa ajeitar busca

--- user-countries/frontend/js/script.js
+++ user-countries/frontend/js/script.js
@@ -1,14 +1,46 @@
 let globalUsers = [];
 let globalCountries = [];
 let globalUsersCountries = [];
+let globalFilteredUsersCountries = [];
 
 async function start() {
-  await fetchUsers();
-  await fetchCountries();
+  //normal
+  // await fetchUsers();
+  // await fetchCountries();
+
+  //Promise sequencial
+  //await promiseCountries();
+  //await promiseUsers();
+
+  //promise paralela
+  const p1 = promiseUsers();
+  const p2 = promiseCountries();
+
+  await Promise.all([p1, p2]);
 
   hideSpinner();
   mergeUsersAndCountries();
   render();
+  configFilter();
+}
+
+function promiseUsers() {
+  return new Promise(async (resolve, reject) => {
+    const users = await fetchUsers();
+    setTimeout(() => {
+      console.log("promise resolvida");
+      resolve(users);
+    }, 1000);
+  });
+}
+function promiseCountries() {
+  return new Promise(async (resolve, reject) => {
+    const countries = await fetchCountries();
+    setTimeout(() => {
+      console.log("promise resolvida");
+      resolve(countries);
+    }, 500);
+  });
 }
 async function fetchUsers() {
   const resource = await fetch("http://localhost:3002/users");
@@ -53,14 +85,16 @@ function mergeUsersAndCountries() {
       countryFlag: country.countryFlag,
     });
   });
-  console.log(globalUsersCountries);
+
+  globalUsersCountries.sort((a, b) => a.userName.localeCompare(b.userName));
+  globalFilteredUsersCountries = [...globalUsersCountries];
 }
 function render() {
   const divUsers = document.querySelector("#users");
   divUsers.innerHTML = `
     <div class='row'>
-        ${globalUsersCountries.map(
-          ({ countryFlag, userPicture, userName, countryName }) => {
+        ${globalUsersCountries
+          .map(({ countryFlag, userPicture, userName, countryName }) => {
             return `
             <div class='col s6 m4 l3'>
                 <div class="flex-row bordered">
@@ -72,10 +106,31 @@ function render() {
               </div>
             </div>
             `;
-          }
-        )}
+          })
+          .join("")}
     </div>
     `;
+}
+function configFilter() {
+  const buttonFilter = document.querySelector("#buttonFilter");
+  const inputFilter = document.querySelector("#inputFilter");
+
+  inputFilter.addEventListener("keyup", handleFilterKeyUp);
+  buttonFilter.addEventListener("click", handleButtonClick);
+}
+
+function handleButtonClick() {
+  const inputFilter = document.querySelector("#inputFilter");
+  const filterValue = inputFilter.value.toLowerCase().trim();
+
+  globalFilteredUsersCountries = globalUsersCountries.filter((item) => {
+    return item.userName.toLowerCase().includes(filterValue);
+  });
+  render();
+}
+function handleFilterKeyUp({key}) {
+  if (key !== "Enter") return;
+  handleButtonClick();
 }
 
 start();
