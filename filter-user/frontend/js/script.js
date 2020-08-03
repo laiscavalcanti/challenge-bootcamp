47c047fa23f0bb1579a274590d49286dec4ef72b
laiscavalcanti <cavalcantislais@gmail.com>
2020-08-02 23:43:54 -0300

    filtro usuarios feito

--- filter-user/frontend/js/script.js
+++ filter-user/frontend/js/script.js
@@ -1,29 +1,44 @@
 let inputFilter = null,
-    buttonFilter = null,
-    panelUsers = null,
-    panelStatistics = null;
+  buttonFilter = null,
+  panelUsers = null,
+  panelStatistics = null;
+users = [];
 
-window.addEventListener('load', () =>{
+window.addEventListener("load", async () => {
   mapElements();
-  await promiseUsers();
+  await fetchUsers();
 
+  addEvents();
+});
 
-})
-
-function mapElements(){
+function mapElements() {
   inputFilter = document.querySelector("#inputFilter");
   buttonFilter = document.querySelector("#buttonFilter");
   panelUsers = document.querySelector("#users");
   panelStatistics = document.querySelector("#statistics");
 }
 
-async function start() {
-  await promiseUsers();
-  hideSpinner();
-  handleButtonClick();
+async function fetchUsers() {
+  const resource = await fetch("http://localhost:3003/users");
+  const json = await resource.json();
+  users = json
+    .map(({ login, name, dob, gender, picture }) => {
+      const fullName = `${name.first} ${name.last}`;
+      return {
+        id: login.uuid,
+        name: fullName,
+        nameLowerCase: fullName.toLowerCase(),
+        age: dob.age,
+        gender: gender,
+        picture: picture.large,
+      };
+    })
+    .sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+  console.log(users);
 }
-
-function promiseUsers() {
+/*function promiseUsers() {
   return new Promise(async (resolve, reject) => {
     const users = await fetchUsers();
     setTimeout(() => {
@@ -31,91 +46,71 @@ function promiseUsers() {
       resolve(users);
     }, 1000);
   });
-}
-async function fetchUsers() {
-  const resource = await fetch("http://localhost:3003/users");
-  const json = await resource.json();
-  users = json.results;
-
-  globalUsers = json.map(({ name, picture, dob, gender, login }) => {
-    return {
-      userId: login.uuid,
-      userName: name.first,
-      userAge: dob.age,
-      userPicture: picture.medium,
-      userGerder: gender,
-    };
-  });
-  console.log(globalUsers);
-  globalUsers.sort((a => a.userName));
-  globalFilteredUsers = [...globalUsers];
-}
-function hideSpinner() {
-  const spinner = document.querySelector("#spinner");
-  spinner.classList.add("hide");
-}
-
+}*/
 
 function addEvents() {
- 
-    inputFilter.addEventListener('keyup', handleKeyUp);
+  inputFilter.addEventListener("keyup", handleKeyUp);
 }
 
-function handleKeyUp(event){
+function handleKeyUp(event) {
   const currentKey = event.key;
 
-    if(currentKey !== event.key){
-      return;
-    }
-  const filterText = event.target.value
-  console.log(filterText);
+  if (currentKey !== "Enter") {
+    return;
+  }
+  const filterText = event.target.value;
+
+  if (filterText.trim() !== "") {
+    filterUsers(filterText);
+  }
 }
-  const resultsUsers = document.querySelector("#resultsUsers");
-  resultsUsers.innerHTML = `${globalFilteredUsers.length} usuário(s) encontrado(s)`;
-  divUsers.innerHTML = `
-            <div class= 'row'>
-           ${globalFilteredUsers
-             .map(({ userName, userAge, userGerder }) => {
-               
-               // console.log(userGender.length)
-               return `
-                <div>
-                    <span>${userName}</span>
-                    <span>${userAge}</span>
-                    <span>${userGerder}</span>
-                </div>
-          
-                
-                `;
-             })
-             .join("")}
-          
-            </div>
-        `;
-}
-/*function renderStatistics(){
-  const divStatistics = document.querySelector("#statistics");
-  //const filterGender = [d => d.userGender ==='male'];
-  //console.log(filterGender.length)
-       divStatistics.innerHTML = `
-           <div class= 'row'> 
-           ${globalFilteredUsers
-           </div>
-           `
-    }*/
 
-function handleButtonClick() {
-
-  buttonFilter.addEventListener("click", () => {
-    
-    const filterValue = inputFilter.value.toLowerCase().trim();
-
-    globalFilteredUsers = globalUsers.filter((item) => {
-      return item.userName.toLowerCase().includes(filterValue);
-    });
-
-    renderUsers();
-    //renderStatistics();
+function filterUsers(filterText) {
+  const filterTextLowerCase = filterText.toLowerCase();
+  const filteredUsers = users.filter((user) => {
+    return user.nameLowerCase.includes(filterTextLowerCase);
   });
+
+  renderUsers(filteredUsers);
+  renderStatistics(filteredUsers);
 }
-start();
+
+function renderUsers(users) {
+  panelUsers.innerHTML = "";
+
+  const h2 = document.createElement("h2");
+  h2.textContent = `${users.length} usuário(s) encontrado(s)`;
+  const ul = document.createElement("ul");
+
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    const img = `<img class="avatar" src="${user.picture}" alt="${user.name}" />`;
+    const userData = `<span>${user.name} ${user.age}</span>`
+
+    li.innerHTML = `<span>${img} ${userData}</span>`;
+    ul.appendChild(li);
+  });
+
+  panelUsers.appendChild(h2);
+  panelUsers.appendChild(ul);
+}
+
+function renderStatistics(users) {
+  const countMale = users.filter((user) => user.gender === "male").length;
+  const countFemale = users.filter((user) => user.gender === "female").length;
+
+  const sumAges = users.reduce((acc, crr) => {
+    return acc + crr.age;
+  }, 0);
+
+  const avaregeAges = Math.round(sumAges / users.length);
+  panelStatistics.innerHTML = `<h2>Estatísticas</h2>
+   
+    <ul>
+      <li>Sexo M <strong>${countMale}</strong></li>
+      <li>Sexo F <strong>${countFemale}</strong></li>
+      <li>Soma idades Homens <strong>${sumAges}</strong></li>
+      <li>Soma idade Mulheres <strong>${avaregeAges}</strong></li>
+    </ul>
+  `;
+}
